85ecfda234ecc6d1e2c58a7fd3982fe83567bda2
Jagan Mohan <jagz5246@gmail.com>
2023-03-24 14:38:53 +0000

    set dark mode as default

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import xd from './Icons/xd.png';
 
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <div className={darkMode?"dark":""}>
         <main className=' bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
